0560ad06b3214de50fd796a9e31a6a27371a577e
Renato Antonio Santos <renato.antonio7@hotmail.com>
2020-06-28 14:07:06 -0300

    Primeiros Arquivos

---
+++ Exercicio1/Exercicio1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main()
+{
+	std::cout << "EU" <<std::endl;
+	std::cout << "VOU"<< "\n";
+	std::cout << "APRENDER" << std::endl;
+	std::cout << "C++" << "\n";
+	system("PAUSE");
+	system("color 04");
+	return 0;
+
+	//Codigo feito por RENATO
+	/*O meu codigo exibe o texto
+	EU VOU APRENDER C++*/
+
+
+}
---
+++ PrimeiroCodigo/Primeiro.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main()
+{
+	std::cout << "Meu nome e Renato tenho 26 anos e estou comecando o Curso de C++" << std::endl;
+	system("PAUSE");
+	system("color F5");
+	return 0;
+}
